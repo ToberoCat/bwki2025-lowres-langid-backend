c4ced8712fc12d8047526bf4347b6dd643aa86e9
Tobias Madlberger <tobias.madlberger@gmail.com>
2025-09-20 14:58:03 +0200

    removed fetch health script

--- fetch-health.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-URL="https://bwki2025-lowres-langid-backend.fly.dev/health"
-
-while true; do
-  echo "$(date): $(curl -s -o /dev/null -w "%{http_code}" "$URL")"
-  sleep 30
-done
